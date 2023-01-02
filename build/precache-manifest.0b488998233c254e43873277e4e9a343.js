4245766e9aea600004654d1fca2c3a90884f4e5a
ciobiano <rafaelobiano@yahoo.com>
2023-01-02 02:32:06 +0100

    update

---
+++ build/precache-manifest.0b488998233c254e43873277e4e9a343.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "afe35643a630ab82a8aa",
+    "url": "/OpenIntel/static/css/main.2c896e52.chunk.css"
+  },
+  {
+    "revision": "afe35643a630ab82a8aa",
+    "url": "/OpenIntel/static/js/main.0ec5886c.chunk.js"
+  },
+  {
+    "revision": "dbef18bea5b044a285a6",
+    "url": "/OpenIntel/static/js/runtime~main.7669f6e6.js"
+  },
+  {
+    "revision": "8cd8995c4ffc4a6f02fc",
+    "url": "/OpenIntel/static/js/2.b3dca170.chunk.js"
+  },
+  {
+    "revision": "c32c8d7dd2ff799c3ae678012cdd144f",
+    "url": "/OpenIntel/static/media/logo.c32c8d7d.svg"
+  },
+  {
+    "revision": "8669bd6263dac99977dd72e2e7e7f17b",
+    "url": "/OpenIntel/static/media/blog01.8669bd62.jpeg"
+  },
+  {
+    "revision": "58ae74c8aa102f47b692b659a29901f1",
+    "url": "/OpenIntel/static/media/blog02.58ae74c8.jpg"
+  },
+  {
+    "revision": "3f8c0fc6c08930626effb8ff3ed18948",
+    "url": "/OpenIntel/static/media/blog03.3f8c0fc6.png"
+  },
+  {
+    "revision": "1cef8610dec40cea6eccdaef88c3744d",
+    "url": "/OpenIntel/static/media/blog04.1cef8610.jpg"
+  },
+  {
+    "revision": "754bc73011e057dc76e55a44a954e0c3",
+    "url": "/OpenIntel/static/media/blog05.754bc730.png"
+  },
+  {
+    "revision": "eb64628456bd4d973cd0ef537b614b34",
+    "url": "/OpenIntel/static/media/blog06.eb646284.png"
+  },
+  {
+    "revision": "793ee30363f717fc11a704858ae1705a",
+    "url": "/OpenIntel/static/media/blog07.793ee303.jpg"
+  },
+  {
+    "revision": "8cb8fd2fb6b6379a7b2cf53a67540f5c",
+    "url": "/OpenIntel/static/media/blog08.8cb8fd2f.png"
+  },
+  {
+    "revision": "625313ec7120cd5a7d8b107a1204ac9b",
+    "url": "/OpenIntel/static/media/Meta.625313ec.svg"
+  },
+  {
+    "revision": "0d57caf3f14f3f664f291372f25e1c40",
+    "url": "/OpenIntel/static/media/OpenAI.0d57caf3.svg"
+  },
+  {
+    "revision": "3d84899326ac5f0f66149fa037693d18",
+    "url": "/OpenIntel/static/media/people.3d848993.png"
+  },
+  {
+    "revision": "84a236ec4136a990d431b1ece9a8784a",
+    "url": "/OpenIntel/static/media/technology-icon.84a236ec.svg"
+  },
+  {
+    "revision": "a624a5e5eb56998301f727a3c78f8b0f",
+    "url": "/OpenIntel/static/media/possibility.a624a5e5.png"
+  },
+  {
+    "revision": "03dab9b952d10d11e19b36c52cb7fbad",
+    "url": "/OpenIntel/index.html"
+  }
+];
